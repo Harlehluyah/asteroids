b525dd9333654ab65b61f477090e4828b00e8d9a
Harlehluyah <marrcuss00@gmail.com>
2024-12-20 18:04:04 +0800

    5. added circleshape.py, Player.py, PLAYER_RADIUS in constants.py, players tirangle on screen

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
 import pygame
 from constants import *
+from Player import *
 
 def main():
     pygame.init()
     print("Starting asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
-    
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
     clock = pygame.time.Clock()
     dt = 0
+    
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2) # added in 5.
     
     while(True):
         for event in pygame.event.get(): # x-button to work on the window
             if event.type == pygame.QUIT:
                 return 
         screen.fill("black") 
+        player.draw(screen) # added in 5.
         pygame.display.flip()
         
         dt = clock.tick(60) / 1000
+        
+        
         
     
 
