45c251c18e375c413af8de5c2184851b0837f643
Harlehluyah <marrcuss00@gmail.com>
2024-12-20 21:48:37 +0800

    12. limit shooting rate

--- Player.py
+++ Player.py
@@ -1,12 +1,13 @@
 from circleshape import *
 from Shot import Shot
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS, PLAYER_SHOOT_COOLDOWN
 
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
         self.add(*self.containers) # added in 8.
+        self.timer = 0 # added in 12. (CH3 - 4)
     
     # in the player class
     def triangle(self):
@@ -36,12 +37,20 @@ class Player(CircleShape):
             self.move(dt * -1)
         if keys[pygame.K_SPACE]: # added in 11. (CH3 - 3)
             self.shoot()
+        
+        self.timer -= dt # added in 12. (CH3 - 4)
     
     def move(self, dt): # added in 7
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
     
-    def shoot(self): # added in 11. (CH3 - 3)
-        new_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
-        new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+    def shoot(self): # added in 11. (CH3 - 3) modified in (CH3 - 4)
+        if self.timer <= 0: # added in 12. (CH3 - 4)
+            new_shot = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+            new_shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
+            self.timer = PLAYER_SHOOT_COOLDOWN 
+        
+        
+        
+            
     
